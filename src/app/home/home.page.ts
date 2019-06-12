1f3d866ff91f8072647a23d27a2db6fb870b8d16
Edwin David Casa Velasco <DavidCasa01>
2019-06-12 09:05:03 -0500

    initial commit, with all changes and implements

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,32 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  image: string = null;
+  //images: string = null;
+  images= [];
+  constructor(private camera: Camera) {}
 
+  getPicture(){
+    let options: CameraOptions = {
+      destinationType : this.camera.DestinationType.DATA_URL,
+      targetWidth: 1000,
+      targetHeight: 1000,
+      quality: 100,
+      saveToPhotoAlbum: true
+    }
+    this.camera.getPicture( options )
+    .then(ImageData => {
+      console.log("ingresando a la funcion");
+     // this.image.concat(`data:image/jpeg;base64,${ImageData}`);
+     this.image = `data:image/jpeg;base64,${ImageData}`;
+     var temp = {path:this.image};
+     console.log(temp);
+     this.images.push(temp);
+     console.log("LOG IMG"+this.images);
+    })
+    .catch( error => {
+      console.log(error); 
+    }
+    )
+  }
 }
